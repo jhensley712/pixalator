1c9759e845df71db75f9395d50fe07c44879d76d
JP <41178108+catd0gg@users.noreply.github.com>
2019-11-03 14:23:04 -0500

    removed unnecessary code + cleared up comments

--- app.js
+++ app.js
@@ -6,41 +6,33 @@ var sizeOf = require('image-size');
 
 // get size of image
 var dimensions = sizeOf('87.jpg');
-console.log(dimensions.width, dimensions.height);
-
-// convert -size 100x100 canvas:khaki  canvas_khaki.gif
-im.convert(['-size', dimensions.width + 'x' + dimensions.height, 'canvas:khaki', 'canvas.jpg'],
-  function(err, stdout) {
-    if (err) throw err;
-    console.log('canvas created: ', stdout);
-  }
-);
+console.log(dimensions.width, "x", dimensions.height);
 
 // chop up image
-im.convert(['87.jpg', '-crop', '160x80@', '+repage', '+adjoin', 'images/87_tiles_%08d.jpg'],
+im.convert(['87.jpg', '-crop', dimensions.width / 5 + 'x' + dimensions.height / 5 + '@', '+repage', '+adjoin', 'images/87_tiles_%08d.jpg'],
 function(err, stdout) {
   if (err) throw err;
   else {
     // if successful, alert user and get avg color of each image
-    console.log('step 1: ', stdout);
+    console.log('step 1 (image chopped): ', stdout);
     // resize each image to 1x1px so that each smaller image is the average color of each original image
     im.convert(['images/87_tiles_*.jpg', '-resize', '1x1', 'images/87_tiles_%08d.jpg'],
     function(err, stdout){
       if (err) throw err;
       else {
         // if successful, resize to 100x100
-        console.log('step 2: ', stdout);
+        console.log('step 2 (images shrunk): ', stdout);
         im.convert(['images/87_tiles_*.jpg', '-resize', '5x5', 'images/87_tiles_%08d.jpg'],
         function(err, stdout){
           if (err) throw err;
           else {
             // finally, recompile
-            console.log('step 3: ', stdout);
+            console.log('step 3 (images resized): ', stdout);
             console.log('shrunk');
             im.montage(['-tile', '160x80', '-geometry', '5x5', '-mode', 'concatenate', 'images/87_tiles_*.jpg', '87_rejoined.jpg'],
             function(err, stdout) {
               if (err) throw err;
-              console.log('step 4: ', stdout);
+              console.log('step 4 (image rejoined): ', stdout);
             });
           }
         });
